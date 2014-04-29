8474991a4e6d46145c90bc8ea2b746d2693538ef
Ivan Remen <bhyk@yandex.ru>
2014-04-30 01:19:19 +0400

    Designition test done

--- tests/designation_test.py
+++ tests/designation_test.py
@@ -5,7 +5,14 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('127.0.0.1', 27017))
 s.listen(1)
 while True:
+    domain1 = "mail.ru"
+    domain2 = "ya.ru"
+    domain1_len = len(domain1)
+    domain2_len = len(domain2)
+    total_packet_len = 4 + 2 + 2 + domain1_len + domain2_len
     conn, addr = s.accept()
-    conn.send("tesk done")
+    conn.send(struct.pack("!IHH", total_packet_len, domain1_len, domain2_len))
+    conn.send(domain1)
+    conn.send(domain2)
     conn.close()
 s.close()
